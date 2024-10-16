6720f28c94c669f3cfec671167140def419b8e94
Manodeepray <127145195+Manodeepray@users.noreply.github.com>
2024-10-17 00:50:15 +0530

    initial commit

--- processing/loading_dataset.py
+++ processing/loading_dataset.py
@@ -2,10 +2,10 @@ from datasets import load_dataset
 
 
 def load_docs():
-    corpus = load_dataset(path= "BeIR/nq", name = "corpus" ,data_dir = 'datasets' )
+    corpus = load_dataset(path= "BeIR/nq", name = "corpus" )
 
 
-    queries = load_dataset(path = "BeIR/nq", name = "queries" , data_dir = 'datasets')
+    queries = load_dataset(path = "BeIR/nq", name = "queries")
     # Load Natural Questions dataset from HuggingFace
     # dataset = load_dataset("natural_questions", split='train[:1%]')  # For example, take 1% of the training data
 
